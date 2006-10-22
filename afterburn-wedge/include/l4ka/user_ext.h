4d194a86f3f56d8bcc3686f9fbffe2270a35e694
Jan Stoess <stoess@ira.uka.de>
2006-10-22 07:27:15 +0200

    Some new files.

---
+++ afterburn-wedge/include/l4ka/user_ext.h
@@ -0,0 +1,180 @@
+/*********************************************************************
+ *
+ * Copyright (C) 2005,  University of Karlsruhe
+ *
+ * File path:     afterburn-wedge/include/l4-common/user.h
+ * Description:   Data types for mapping L4 threads to guest OS threads.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: user.h,v 1.9 2005/09/05 14:10:05 joshua Exp $
+ *
+ ********************************************************************/
+#ifndef __L4KA__USER_EXT_H__
+#define __L4KA__USER_EXT_H__
+
+#include <l4/ia32/arch.h>
+#include <l4/ipc.h>
+#include INC_ARCH(cpu.h)
+#include INC_ARCH(page.h)
+#include INC_ARCH(types.h)
+#include INC_WEDGE(debug.h)
+
+#define OFS_MR_SAVE_TAG		 0
+#define OFS_MR_SAVE_EXC_NO	 1
+#define OFS_MR_SAVE_ERRCODE	 2
+#define OFS_MR_SAVE_EIP		 4
+#define OFS_MR_SAVE_EFLAGS	 5 
+#define OFS_MR_SAVE_EDI		 6 
+#define OFS_MR_SAVE_ESI		 7  
+#define OFS_MR_SAVE_EBP		 8 
+#define OFS_MR_SAVE_ESP		 9 
+#define OFS_MR_SAVE_EBX		10 
+#define OFS_MR_SAVE_EDX		11 
+#define OFS_MR_SAVE_ECX		12 
+#define OFS_MR_SAVE_EAX		13 
+
+enum thread_state_t { 
+    thread_state_pfault, 
+    thread_state_exception,
+    thread_state_preemption
+};
+class mr_save_t
+{
+private:
+    union 
+    {
+	L4_Word_t raw[CTRLXFER_SIZE+3];
+	struct {
+	    L4_MsgTag_t tag;
+	    union 
+	    {
+		union {
+		    struct {
+			L4_Word_t addr;
+			L4_Word_t ip;
+		    };
+		    L4_MapItem_t item;
+		} pfault;
+		struct {
+		    L4_Word_t excno;
+		    L4_Word_t errcode;
+		} exception;
+		struct {
+		    L4_Word_t time1;
+		    L4_Word_t time2;
+		} preempt;
+
+	    };
+	    L4_CtrlXferItem_t ctrlxfer;
+	};
+    };
+public:
+    void init(){ tag.raw = 0; }
+    
+    L4_Word_t get(word_t idx)
+	{
+	    ASSERT(idx < (CTRLXFER_SIZE+3));
+	    return raw[idx];
+	}
+    void set(word_t idx, word_t val)
+	{
+	    ASSERT(idx < (CTRLXFER_REG_SIZE+3));
+	    raw[idx] = val;
+	}
+
+    void store_mrs(L4_MsgTag_t t) 
+	{	
+	    ASSERT (L4_UntypedWords(t) == 2);
+	    ASSERT (L4_TypedWords(t) == CTRLXFER_SIZE);
+	    L4_StoreMR( 0, &raw[0] );
+	    L4_StoreMR( 1, &raw[1] );
+	    L4_StoreMR( 2, &raw[2] );
+	    L4_StoreCtrlXferItem(3, &ctrlxfer);
+	}
+    void load_mrs(word_t idx=1) 
+	{
+	    ASSERT (L4_UntypedWords(tag) + L4_TypedWords(tag) 
+		    <= (CTRLXFER_SIZE+3-idx));
+	    L4_LoadMR(  0 , tag.raw);
+	    L4_LoadMRs( 1, 
+		    L4_UntypedWords(tag) + L4_TypedWords(tag),
+		    &raw[idx] );
+	}
+
+    void load_pfault_reply(L4_MapItem_t map_item) 
+	{
+	    tag.X.u = 0;
+	    tag.X.t = 2 + CTRLXFER_SIZE;
+	    pfault.item = map_item;
+	    L4_InitCtrlXferItem(&ctrlxfer, 0x3ff);
+	    load_mrs();
+	}
+
+    void load_exception_reply(iret_handler_frame_t *iret_emul_frame) 
+	{
+	    for( u32_t i = 0; i < 9; i++ )
+		ctrlxfer.regs[i+1] = iret_emul_frame->frame.x.raw[8-i];
+	    ctrlxfer.eip = iret_emul_frame->iret.ip;
+	    ctrlxfer.esp = iret_emul_frame->iret.sp;
+	    
+	    tag.X.u = 0;
+	    tag.X.t = CTRLXFER_SIZE;
+	    L4_InitCtrlXferItem(&ctrlxfer, 0x3ff);
+	    load_mrs(3);
+
+	}
+    void load_startup_reply(iret_handler_frame_t *iret_emul_frame) 
+	{ load_exception_reply(iret_emul_frame); }
+    void load_preemption_reply() 
+	{ 
+	    tag.X.u = 0;
+	    tag.X.t = CTRLXFER_SIZE;
+	    L4_InitCtrlXferItem(&ctrlxfer, 0x3ff);
+	    load_mrs(3);
+	}
+
+    L4_MsgTag_t get_msg_tag() { return tag; }
+    void set_msg_tag(L4_MsgTag_t t) { tag = t; }
+    void set_propagated_reply(L4_ThreadId_t virtualsender) 
+	{ 
+	    L4_Set_Propagation(&tag); 
+	    L4_Set_VirtualSender(virtualsender);
+	}
+
+    
+    L4_Word_t get_pfault_ip() { return ctrlxfer.eip; }
+    L4_Word_t get_pfault_addr() { return pfault.addr; }
+    L4_Word_t get_pfault_rwx() { return L4_Label(tag) & 0x7; }
+
+    L4_Word_t get_exc_ip() { return ctrlxfer.eip; }
+    void set_exc_ip(word_t ip) { ctrlxfer.eip = ip; }
+    L4_Word_t get_exc_sp() { return ctrlxfer.esp; }
+
+    L4_Word64_t get_preempt_time() 
+	{ return ((L4_Word64_t) preempt.time2 << 32) | ((L4_Word64_t) preempt.time1); }
+
+    
+};
+
+
+#endif /* !__L4KA__USER_EXT_H__ */
---
+++ afterburn-wedge/l4ka/irqext.cc
@@ -0,0 +1,301 @@
+/* (C) 2005,  University of Karlsruhe
+ *
+ * File path:     afterburn-wedge/l4-common/irq.cc
+ * Description:   The irq thread for handling asynchronous events.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: irq.cc,v 1.28 2006/01/11 19:01:17 stoess Exp $
+ *
+ ********************************************************************/
+
+#include <l4/ipc.h>
+#include <l4/kip.h>
+#include <l4/schedule.h>
+
+#include INC_ARCH(intlogic.h)
+#include INC_WEDGE(console.h)
+#include INC_WEDGE(vcpulocal.h)
+#include INC_WEDGE(backend.h)
+#include INC_WEDGE(l4privileged.h)
+#include INC_WEDGE(hthread.h)
+#include INC_WEDGE(vm.h)
+#include INC_WEDGE(irq.h)
+#include INC_WEDGE(message.h)
+
+#include <device/acpi.h>
+
+static const bool debug_hwirq=0;
+static const bool debug_timer=0;
+static const bool debug_virq=0;
+static const bool debug_ipi=0;
+static const bool debug_preemption=0;
+
+static unsigned char irq_stack[CONFIG_NR_VCPUS][KB(16)] ALIGNED(CONFIG_STACK_ALIGN);
+static const L4_Clock_t timer_length = {raw: 10000};
+
+static void irq_handler_thread( void *param, hthread_t *hthread )
+{
+    L4_Word_t tid_user_base = L4_ThreadIdUserBase(L4_GetKernelInterface());
+    vcpu_t &vcpu = get_vcpu();
+    
+    intlogic_t &intlogic = get_intlogic();
+    local_apic_t &lapic = get_lapic();
+
+    L4_ThreadId_t tid = L4_nilthread;
+    L4_ThreadId_t ack_tid = L4_nilthread;
+    L4_Error_t errcode;
+    
+    L4_Word_t irq, vector;
+
+    con << "IRQ thread, "
+	<< "TID " << hthread->get_global_tid() 
+	<< "\n";      
+
+    // Set our thread's exception handler. 
+    L4_Set_ExceptionHandler( get_vcpu().monitor_gtid );
+    
+    
+    /*
+     * Associate with virtual timing source
+     * jsXXX: maybe do that one VAPIC timers are enabled
+     */
+    tid.global.X.thread_no = INTLOGIC_TIMER_IRQ;
+    tid.global.X.version = 1;
+    errcode = AssociateInterrupt( tid, L4_Myself() );
+    if(debug_timer || intlogic.is_irq_traced(irq)) 
+	con << "enable virtual timer irq: " 
+	    << INTLOGIC_TIMER_IRQ << '\n';
+    if( errcode != L4_ErrOk )
+	con << "Unable to associate virtual timer interrupt: "
+	    << irq << ", L4 error: " 
+	    << L4_ErrString(errcode) << ".\n";
+    
+    for (;;)
+    {
+	L4_MsgTag_t tag = L4_ReplyWait( ack_tid, &tid );
+	
+	if( L4_IpcFailed(tag) )
+	{
+	    errcode = L4_ErrorCode();
+	    con << "IRQ thread timeout"
+		<< " to thread" << ack_tid  
+		<< " from thread" << tid
+		<< " error " << (void *) errcode
+		<< "\n";
+	    L4_KDB_Enter("VMEXT IRQ Failure\n");
+	    ack_tid = L4_nilthread;
+	    continue;
+	}
+	
+	ack_tid = L4_nilthread;
+	// Received message.
+	switch( L4_Label(tag) )
+	{
+	    case msg_label_hwirq:
+	    {
+		// Hardware IRQ.
+		if (tid.global.X.thread_no < tid_user_base )
+		{
+		    ASSERT(CONFIG_DEVICE_PASSTHRU);
+		    irq = tid.global.X.thread_no;
+		    if(debug_hwirq || intlogic.is_irq_traced(irq)) 
+			con << "hardware irq: " << irq
+			    << ", int flag: " << get_cpu().interrupts_enabled()
+			    << '\n';
+		    
+		    /* 
+		     * jsXXX: not strictly necessary here, 
+		     * pic/apic will set it afterwards as well
+		     */
+		    intlogic.set_hwirq_mask(irq);
+
+		}
+		else
+		{
+		    /* jsXXX: check vtimer irq source here */
+		    irq = INTLOGIC_TIMER_IRQ;
+		    if(debug_hwirq || intlogic.is_irq_traced(irq)) 
+		    {
+			static L4_Clock_t last_time = {raw: 0};
+			L4_Clock_t current_time = L4_SystemClock();
+			L4_Word64_t time_diff = (current_time - last_time).raw;
+			
+			if(debug_timer || intlogic.is_irq_traced(irq)) 
+			    con << "vtimer irq: " << irq
+				<< " diff " << (L4_Word_t) time_diff
+				<< " int flag: " << get_cpu().interrupts_enabled() 
+				<< "\n";
+			last_time = current_time;
+		    }
+		}
+		intlogic.raise_irq( irq );
+		break;
+	    }
+	    case msg_label_hwirq_ack:
+	    {
+		ASSERT(CONFIG_DEVICE_PASSTHRU);
+		msg_hwirq_ack_extract( &irq );
+		if(debug_hwirq || intlogic.is_irq_traced(irq))
+		    con << "hardware irq ack " << irq << '\n';
+		ack_tid.global.X.thread_no = irq;
+		ack_tid.global.X.version = 1;
+		L4_LoadMR( 0, 0 );  // Ack msg.
+		break;
+	    }
+	    case msg_label_virq:
+	    {
+		// Virtual interrupt from external source.
+		msg_virq_extract( &irq );
+		if( debug_virq )
+		    con << "virtual irq: " << irq 
+			<< ", from TID " << tid << '\n';
+		intlogic.raise_irq( irq );
+		break;
+	    }		    
+	    case msg_label_ipi:
+	    {
+		L4_Word_t src_vcpu_id;		
+		msg_ipi_extract( &src_vcpu_id, &vector  );
+		if (debug_ipi) 
+		    con << " IPI from VCPU " << src_vcpu_id 
+			<< " vector " << vector
+			<< '\n';
+		lapic.raise_vector(vector, INTLOGIC_INVALID_IRQ);;
+		    
+#if defined(CONFIG_DEVICE_PASSTHRU)
+		case msg_label_device_enable:
+		    L4_Word_t prio = resourcemon_shared.prio + CONFIG_PRIO_DELTA_IRQ;
+		    msg_device_enable_extract(&irq);
+		    tid.global.X.thread_no = irq;
+		    tid.global.X.version = 1;
+		    errcode = AssociateInterrupt( tid, L4_Myself() );
+		    
+		    if(debug_hwirq || intlogic.is_irq_traced(irq)) 
+			con << "enable device irq: " << irq << '\n';
+		    
+		    if( errcode != L4_ErrOk )
+			con << "Attempt to associate an unavailable interrupt: "
+			    << irq << ", L4 error: " 
+			    << L4_ErrString(errcode) << ".\n";
+		    break;
+	    }		    
+	    case msg_label_device_disable:
+	    {
+		msg_device_disable_extract(&irq);
+		tid.global.X.thread_no = irq;
+		tid.global.X.version = 1;
+		    
+		if(debug_hwirq || intlogic.is_irq_traced(irq)) 
+		    con << "disable device irq: " << irq
+			<< '\n';
+		errcode = DeassociateInterrupt( tid );
+		if( errcode != L4_ErrOk )
+		    con << "Attempt to deassociate an unavailable interrupt: "
+			<< irq << ", L4 error: " 
+			<< L4_ErrString(errcode) << ".\n";
+		    
+		break;
+	    }
+#endif /* defined(CONFIG_DEVICE_PASSTHRU) */
+		    
+#if defined(CONFIG_VSMP)
+	    case msg_label_migrate_uthread:
+	    {
+		L4_ThreadId_t uthread;
+		L4_Word_t dest_vcpu_id, expected_control;		
+		msg_migrate_uthread_extract( &uthread, &dest_vcpu_id, &expected_control);
+		if (debug_ipi) 
+		    con << "request from VCPU " << dest_vcpu_id 
+			<< " to migrate uthread " << uthread
+			<< " expected control " << expected_control
+			<< '\n';
+
+		L4_Word_t old_control, old_sp, old_ip, old_flags;
+		thread_manager_t::get_thread_manager().migrate(uthread, dest_vcpu_id, expected_control,
+			&old_control, &old_sp, &old_ip, &old_flags );
+		ack_tid = get_vcpu(dest_vcpu_id).main_gtid;
+		msg_migrate_uthread_ack_build(uthread, old_control, old_sp, old_ip, old_flags);
+		break;
+	    }
+	    case msg_label_signal_uthread:
+	    {
+		L4_ThreadId_t uthread;
+		L4_Word_t dest_vcpu_id;
+		msg_signal_uthread_extract( &uthread, &dest_vcpu_id);
+		if (debug_ipi) 
+		    con << "request from VCPU " << dest_vcpu_id 
+			<< " to signal uthread " << uthread
+			<< '\n';
+
+		L4_Word_t old_control;
+		thread_manager_t::get_thread_manager().signal(uthread, &old_control );
+		ack_tid = get_vcpu(dest_vcpu_id).main_gtid;
+		msg_signal_uthread_ack_build(uthread, old_control);
+		break;
+	    }
+#endif
+	    case msg_label_preemption:
+	    {
+		if (tid == vcpu.main_gtid)
+		{	
+		    vcpu.main_info.mr_save.store_mrs(tag);
+		    if (debug_preemption)
+			con << "kernel thread sent preemption IPC"
+			    << " tid " << tid << "\n";
+		    
+		    backend_async_irq_deliver( intlogic );
+		    if (debug_preemption)
+			con << "send preemption reply to kernel"
+			    << " tid " << ack_tid << "\n";
+		    ack_tid = vcpu.main_gtid;
+		    vcpu.main_info.mr_save.set_propagated_reply(vcpu.monitor_gtid);
+		    vcpu.main_info.mr_save.load_preemption_reply();
+		    ASSERT(L4_Label(vcpu.main_info.mr_save.get_msg_tag()) ==  msg_label_preemption);
+		    break;
+		}
+	    }			
+	    default:
+		con << "unexpected IRQ message from " << tid << '\n';
+		L4_KDB_Enter("BUG");
+		break;
+	}
+	
+    } /* for (;;) */
+}
+L4_ThreadId_t irq_init( L4_Word_t prio, 
+	L4_ThreadId_t scheduler_tid, L4_ThreadId_t pager_tid,
+	vcpu_t *vcpu )
+{
+    hthread_t *irq_thread =
+	get_hthread_manager()->create_thread( 
+	    (L4_Word_t)irq_stack[vcpu->cpu_id], sizeof(irq_stack),
+	    prio, irq_handler_thread, scheduler_tid, pager_tid, vcpu);
+
+    if( !irq_thread )
+	return L4_nilthread;
+
+    irq_thread->start();
+    
+    return irq_thread->get_local_tid();
+}
+
