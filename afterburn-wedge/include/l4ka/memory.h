776186e8621cbb3054836f3c9b7f0e674bc5969f
Jan Stoess <stoess@ira.uka.de>
2006-11-07 11:47:43 +0100

    New files.

---
+++ afterburn-wedge/include/l4ka/memory.h
@@ -0,0 +1,366 @@
+/*********************************************************************
+ *                
+ * Copyright (C) 2006,  Karlsruhe University
+ *                
+ * File path:     l4ka/memory.h
+ * Description:   
+ *                
+ * @LICENSE@
+ *                
+ * $Id:$
+ *                
+ ********************************************************************/
+#ifndef __L4KA__MEMORY_H__
+#define __L4KA__MEMORY_H__
+
+#include INC_WEDGE(debug.h)
+#include INC_WEDGE(vcpulocal.h)
+#include INC_ARCH(cpu.h)
+
+static const bool debug_device=0;
+
+class ptab_info_t
+{
+private:
+
+    static const word_t ld_max_entries = (PAGE_BITS);
+    static const word_t max_entries = (1UL << ld_max_entries);
+    pgent_t *ptab_bp[max_entries];
+    pgent_t *invalid;
+    pgent_t *cleared;
+    
+
+    word_t hash_ptab_addr( word_t ptab_addr, word_t count, word_t &h1, word_t &h2 )
+	{ 
+	    /* Double multiplicative hashing a la Knuth
+	     * with A = ((sqrt(5)-1)/2) and s = A * 2^32 
+	     */
+	    
+	    if (count) 
+	    {
+		//con << "a " << (void *) ptab_addr 
+		//  << " c " << count
+		//  << " h1 " << h1 
+		//  << " h2 " << h2
+		//  << "\n";
+		return (h1 + count * h2) & (max_entries-1);
+	    }
+	    
+	    const word_t s = 2654435769U;
+	    word_t key = (ptab_addr >> PAGE_BITS);
+	    h1 = h2 = (s * key);
+	    
+	    h1 >>= 32 - ld_max_entries;
+	    h2 >>= (32 - (2 * ld_max_entries));
+	    h2 &= max_entries-1;
+	    h2 |= 1;
+	    
+	    // con << "a " << (void *) ptab_addr 
+	    // << " h1 " << h1 
+	    // << " h2 " << h2
+	    // << "\n";
+	    
+	    return h1;
+	}
+
+    word_t dbg_set;
+    
+    void dbg(const char *prefix, const char *info, pgent_t **cur, word_t ptab_addr, pgent_t *pdir, 
+	    word_t count, bool force=false)
+	{
+	    static const word_t dbg_threshold = 5;
+	    static word_t dbg_count = 0;
+	    
+	    dbg_count++;
+
+	    if (count > dbg_threshold)
+	    {
+		con << prefix 
+		    << " ba " << (void*) ((*cur < invalid) ? (*cur)->get_address() : NULL)		
+		    << " ma " << (void *) ptab_addr
+		    << " bp " << (void *) *cur
+		    << " mp " << (void *) pdir
+		    << " ci " << cur - ptab_bp
+		    << " c "  << count 
+		    << " s "  << dbg_set
+		    << " p "  << dbg_count
+		    << " "    << info 
+		    << "\n"; 
+		
+		dbg_count = 0;
+	    }
+	}
+	    
+    void set( word_t ptab_addr, pgent_t *pdir_entry)
+	{
+	    word_t count = 0;
+	    word_t h1, h2;
+	    pgent_t **cur = NULL;
+	    
+	    for (;;)
+	    {
+		cur = &ptab_bp[hash_ptab_addr(ptab_addr, count, h1, h2)];
+		
+		if (*cur >= invalid)
+		{
+		    /* invalid or cleared entry: break and use */
+		    break;
+		}
+		else if ( (*cur)->get_address() == ptab_addr)
+		    dbg("spd", "shar", cur, ptab_addr, pdir_entry, count, true);
+		
+		
+		//dbg("spd", "test", cur, ptab_addr, pdir_entry, count);
+		if (++count == max_entries)
+		{
+		    ASSERT(false);
+		    return;
+		}
+		
+	    }
+	    
+	    /* 
+	     * We'll do FIFO here, by swapping the most recent and the 
+	     */
+	    
+	    dbg("spd", "done", cur, ptab_addr, pdir_entry, count);
+
+	    *cur = pdir_entry; 
+	    
+	    dbg_set++;
+	}
+
+    void clear( word_t ptab_addr, pgent_t *pdir_entry)
+	{
+	    word_t count = 0;
+	    word_t h1, h2;
+	    pgent_t **cur = NULL;
+	    
+	    for (;;)
+	    {
+		cur = &ptab_bp[hash_ptab_addr(ptab_addr, count, h1, h2)];
+		if (*cur == invalid)
+		{
+		    dbg("cpd", "invl", cur, ptab_addr, pdir_entry, count, true);
+		    /* invalid entry: return */
+		    return;
+		}
+		
+		if (*cur == pdir_entry)
+		{
+		    /* found entry: invalidate and return */
+		    dbg("cpd", "done", cur, ptab_addr, pdir_entry, count);
+		    *cur = cleared;
+		    dbg_set--;
+		    return;
+		}
+
+		/* other entry: continue */
+		//dbg("cpd", "test", cur, ptab_addr, pdir_entry, count);
+				
+		if (++count == max_entries)
+		{
+		    ASSERT(false);
+		    return;
+		}
+	    }
+	}
+
+    bool get( word_t ptab_addr, pgent_t **pdir_entry )
+	{ 
+
+	    word_t count = 0;
+	    word_t h1, h2;
+	    pgent_t **cur = NULL;
+	    
+	    for (;;)
+	    {
+		cur = &ptab_bp[hash_ptab_addr(ptab_addr, count, h1, h2)];
+		if (*cur <= invalid && (*cur)->get_address() == ptab_addr)
+		{
+		    /* found entry: return */
+		    dbg("gpd", "done", cur, ptab_addr, 0, count);
+		    *pdir_entry = *cur;
+		    return true;
+		}
+		
+		else if (*cur == invalid)
+		{
+		    /* invalid entry: we have already deallocated the AS */
+		    dbg("gpd", "invl", cur, ptab_addr, 0, count, true);
+		    return false;
+		}
+
+		//dbg("gpd", "test", cur, ptab_addr, 0, count);
+		
+		if (++count == max_entries)
+		{
+		    ASSERT(false);
+		    return false;
+		}
+	    }
+	}
+
+public:
+    
+    ptab_info_t()
+	{
+	    invalid = (pgent_t *) 0xfffffffe;
+	    cleared = (pgent_t *) 0xffffffff;
+	    
+	    for (word_t i=0; i < max_entries; i++)
+		ptab_bp[i] = invalid;
+	    
+	    dbg_set = 0;
+	}
+
+
+    void update(pgent_t *pdir_entry, pgent_t new_val)
+	{
+	    if (pdir_entry == NULL)
+	    {
+		con << "ptab_info update: pdir NULL\n";
+		return;
+	    }
+	    
+	    if ( pdir_entry->get_raw() == new_val.get_raw())
+		return;
+    
+	    if( pdir_entry->is_valid() && !pdir_entry->is_superpage() )
+		clear(pdir_entry->get_address(), pdir_entry);
+	    
+	    if ( new_val.is_valid() && !new_val.is_superpage())
+		set(new_val.get_address(), pdir_entry);
+
+	}
+    
+    void clear(word_t pdir_paddr)
+	{	
+	    pgent_t *pdir =  (pgent_t *) (pdir_paddr + get_vcpu().get_kernel_vaddr());
+	    
+	    for (word_t i=0; i < max_entries; i++)
+	    {
+		pgent_t *cur_pdir = (pgent_t *) ((word_t) ptab_bp[i] & PAGE_MASK);
+		if (cur_pdir == pdir)
+		{
+		    dbg("cpd", "tdel", &ptab_bp[i], 0, pdir, i, true);
+		    ptab_bp[i] = cleared;
+		    dbg_set--;
+		}
+	    }
+	}
+
+    
+    bool retrieve(pgent_t *pgent, pgent_t **pdir )
+	{
+	    word_t ptab_addr = (word_t) pgent - get_vcpu().get_kernel_vaddr();
+	    ptab_addr &= PAGE_MASK;
+	    return get( ptab_addr, pdir );
+	}
+};
+
+
+extern ptab_info_t ptab_info;
+
+INLINE L4_Word_t unmap_device_mem( L4_Word_t addr, L4_Word_t bits, L4_Word_t rwx=L4_FullyAccessible)
+{
+    L4_Word_t ret = 0;
+    
+    for (word_t pt=0; pt < (1UL << bits) ; pt+= PAGE_SIZE)
+    {
+	L4_Fpage_t fp = L4_FpageLog2( addr + pt, PAGE_BITS);
+	word_t old_attr = 0;
+
+	if (contains_device_mem(addr + pt, addr + pt + PAGE_SIZE - 1))
+	{
+	    old_attr = 7;
+	    backend_unmap_device_mem(addr + pt, PAGE_SIZE, old_attr);		
+	}
+	else
+	    old_attr = L4_Rights(L4_Flush( fp + rwx ));
+	    
+	ret |= old_attr;
+    }
+    return ret;
+}
+
+static const L4_Word_t invalid_pdir_idx = 1024+1;
+
+class unmap_cache_t
+{
+private:
+    static const L4_Word_t cache_size = 60;
+    L4_Fpage_t fpages[cache_size];
+public:
+    L4_Word_t count;
+private:
+    bool flush;
+
+public:
+    void add_mapping( pgent_t *pgent, L4_Word_t bits, word_t pdir_idx, 
+	    L4_Word_t rwx=L4_FullyAccessible, bool do_flush=false )
+	{
+	    /* Check if device memory */
+	    vcpu_t vcpu = get_vcpu();
+	    word_t paddr = pgent->get_address();
+	    word_t kaddr = pgent->get_address() + vcpu.get_kernel_vaddr();
+	 
+	    if (pdir_idx == invalid_pdir_idx)
+	    {
+		pgent_t *pdir;
+		ptab_info.retrieve(pgent, &pdir );
+	    }	    
+	    
+	    if (contains_device_mem(paddr, paddr + (1UL << bits) - 1))
+	    {
+		//jsXXX: virtual APIC/IO-APIC is detected as device memory
+		if (debug_device)
+		{
+		    con << "unmap device_mem" 
+			<< ", addr " << (void*) kaddr
+			<< ", bits " << bits 	
+			<< ", paddr " << (void *) paddr 
+			<< ", ra " << (void *) __builtin_return_address((0))
+			<< "\n";		
+		}
+		unmap_device_mem(paddr, bits, rwx);
+	    }
+	    else 
+	    {
+		flush |= do_flush;
+		if( count == cache_size )
+		    commit();
+		fpages[count++] = L4_FpageLog2( kaddr, bits ) + rwx;
+	    }
+	}
+
+    unmap_cache_t()
+	{ count = 0; flush = false; }
+    
+    word_t commit()
+	{
+	    if( !this->count )
+		return 0;
+
+	    if( flush )
+		L4_FlushFpages( this->count, this->fpages );
+	    else
+		L4_UnmapFpages( this->count, this->fpages );
+    
+	    word_t ret = 0;
+	    for (word_t n=0; n<count;n++)
+		ret |= L4_Rights(this->fpages[n]);
+	
+	    this->count = 0;
+	    this->flush = false;
+	    return ret;
+	}
+
+};
+
+extern unmap_cache_t unmap_cache;
+
+
+
+
+#endif /* !__L4KA__MEMORY_H__ */
---
+++ afterburn-wedge/include/l4ka/sync.h
@@ -0,0 +1,253 @@
+/*********************************************************************
+ *
+ * Copyright (C) 2005,  University of Karlsruhe
+ *
+ * File path:     afterburn-wedge/include/l4ka/vcpu.h
+ * Description:   The per-CPU data declarations for the L4Ka environment.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * $Id: vcpu.h,v 1.19 2006/01/04 15:16:24 joshua Exp $
+ *
+ ********************************************************************/
+#ifndef __L4KA__SYNC_H__
+#define __L4KA__SYNC_H__
+
+#include <l4/kip.h>
+#include <l4/thread.h>
+#include <l4/kdebug.h>
+#include <l4/ipc.h>
+#include INC_ARCH(sync.h)
+
+#undef L4KA_DEBUG_SYNC
+#if defined L4KA_DEBUG_SYNC
+
+
+#define LOCK_DEBUG(cpu, c)					\
+do {								\
+    L4_KDB_PrintChar((char) cpu + '0');				\
+    L4_KDB_PrintChar(c);					\
+ } while (0)
+#else
+#define LOCK_DEBUG(cpu, c)
+#endif
+
+#undef L4KA_ASSERT_SYNC
+#if defined L4KA_ASSERT_SYNC
+#define LOCK_ASSERT(x, c)					\
+    do {							\
+	if(EXPECT_FALSE(!(x))) {				\
+	    L4_KDB_PrintChar('\n');L4_KDB_PrintChar('L');	\
+	    L4_KDB_PrintChar('O');L4_KDB_PrintChar('C');	\
+	    L4_KDB_PrintChar('K');L4_KDB_PrintChar(' ');	\
+	    L4_KDB_PrintChar('A');L4_KDB_PrintChar('S');	\
+	    L4_KDB_PrintChar('S');L4_KDB_PrintChar('E');	\
+	    L4_KDB_PrintChar('R');L4_KDB_PrintChar('T');	\
+	    L4_KDB_PrintChar(' ');L4_KDB_PrintChar('F');	\
+	    L4_KDB_PrintChar('A');L4_KDB_PrintChar('I');	\
+	    L4_KDB_PrintChar('L');L4_KDB_PrintChar('E');	\
+	    L4_KDB_PrintChar('D');L4_KDB_PrintChar('(');	\
+	    L4_KDB_PrintChar(c);L4_KDB_PrintChar(')');		\
+	    L4_KDB_PrintChar('\n');				\
+	    L4_KDB_Enter("Failed Lock Assertion");		\
+	    while (1) ;						\
+	}							\
+    } while(0)							
+#else 
+#define LOCK_ASSERT(x, c)
+#endif
+
+class trylock_t
+{
+public:
+    union
+    {
+	struct 
+	{
+	    u32_t		owner_thread_version	:  7;
+	    u32_t		owner_pcpu_id		:  7;
+	    u32_t		owner_thread_no		: 18;
+	} x;
+	u32_t raw;
+    } lock;	
+    
+    word_t get_raw()
+	{ return lock.raw; }
+    void set_raw_atomic(word_t val)
+	{  
+	    __asm__ __volatile__ (
+		"movl %0, %1"
+		: /* no output */
+		: "r"(val), "m"(lock.raw)
+	    );
+	}
+    L4_ThreadId_t get_owner_tid()
+	{ 
+	    L4_ThreadId_t ret = { raw : 0};
+	    ret.global.X.thread_no = lock.x.owner_thread_no;
+	    ret.global.X.version = lock.x.owner_thread_version;
+	    return ret;	
+	}
+    word_t get_owner_pcpu_id()
+	{ return lock.x.owner_pcpu_id; }
+    
+    void set_owner_tid(L4_ThreadId_t tid)
+	{ 
+	    LOCK_ASSERT(tid.global.X.version < 128, 'V');
+	    lock.x.owner_thread_no = tid.global.X.thread_no; 
+	    lock.x.owner_thread_version = tid.global.X.version; 
+	}
+    void set_owner_pcpu_id(word_t pcpu_id)
+	{ lock.x.owner_pcpu_id = pcpu_id;}
+    
+};
+
+class cpu_lock_t
+{
+private:
+    trylock_t cpulock;
+    
+    bool trylock(L4_ThreadId_t tid, word_t pcpu_id, L4_ThreadId_t *old_tid, word_t *old_pcpu_id )
+	{
+	    bool ret;
+	    
+	    trylock_t new_lock;
+	    new_lock.set_owner_pcpu_id(pcpu_id);
+	    new_lock.set_owner_tid(tid);
+
+	    trylock_t empty_lock;
+	    empty_lock.set_owner_pcpu_id(max_pcpus);
+	    empty_lock.set_owner_tid(L4_nilthread);
+
+	    ret = cmpxchg_ext(&cpulock, empty_lock, &new_lock);
+	    
+	    *old_pcpu_id = new_lock.get_owner_pcpu_id();
+	    *old_tid = new_lock.get_owner_tid();
+	    return ret;
+	}
+
+    void release()
+	{ 
+	    trylock_t new_lock_val;
+	    new_lock_val.set_owner_tid(L4_nilthread);
+	    new_lock_val.set_owner_pcpu_id(max_pcpus);
+	    cpulock.set_raw_atomic(new_lock_val.get_raw());
+	}
+    
+    
+public:
+    static word_t	max_pcpus;
+#if defined L4KA_DEBUG_SYNC
+    static L4_Word_t debug_pcpu_id;
+    static L4_ThreadId_t debug_tid;
+#endif
+    void init()
+	{ 
+	    max_pcpus = L4_NumProcessors(L4_GetKernelInterface());
+    	    cpulock.set_owner_pcpu_id(max_pcpus);
+	    cpulock.set_owner_tid(L4_nilthread);
+	    LOCK_ASSERT(sizeof(cpu_lock_t) == 4, '0');
+	}
+
+    
+    bool is_locked_by_tid(L4_ThreadId_t owner)
+	{
+	    return (cpulock.get_owner_tid() == owner);
+	}
+    
+    bool is_locked_by_cpu(L4_Word_t pcpu_id)
+	{
+	    return (cpulock.get_owner_pcpu_id() == pcpu_id);
+	}
+    
+    bool is_locked()
+	{
+	    return (cpulock.get_owner_pcpu_id() != max_pcpus);
+	}
+
+    void lock()
+	{
+	    /*
+	     * Logic: 
+	     *  owner_pcpu_id == CONFIG_NR_VCPUS : lock is free -> get it
+	     *  owner_pcpu_id == my_vcpu_id : lock held by my VCPU -> donate time
+	     *  owner_pcpu_id == pcpu_id : lock held by VCPU vcpu_id -> spin
+	     * 
+	     */
+	restart:
+	    
+	    word_t new_pcpu_id = L4_ProcessorNo();
+	    L4_ThreadId_t new_tid = L4_Myself();
+
+	    word_t old_pcpu_id = max_pcpus;
+	    L4_ThreadId_t old_tid = L4_nilthread;
+	    
+	    if (!trylock(new_tid, new_pcpu_id, &old_tid, &old_pcpu_id))
+	    {
+#if defined(L4KA_DEBUG_SYNC)
+		debug_tid = old_tid;
+		debug_pcpu_id = old_pcpu_id;
+#endif
+		LOCK_ASSERT(old_tid != L4_nilthread, '1');
+		LOCK_ASSERT(cpulock.get_owner_tid() != L4_Myself(), '2');
+		
+		if (old_pcpu_id == new_pcpu_id)
+		{
+		    LOCK_DEBUG(new_pcpu_id, 'p');
+		    L4_ThreadSwitch(old_tid);
+		}
+		else 
+		{
+		    LOCK_DEBUG(new_pcpu_id, 'q');
+		    while (is_locked_by_cpu(old_pcpu_id))
+			memory_barrier();
+		}
+		goto restart;
+	    }
+	    else 
+	    {
+		LOCK_ASSERT(old_pcpu_id == max_pcpus, '3');
+		LOCK_ASSERT(old_tid == L4_nilthread, '4');
+	    }
+	    LOCK_DEBUG(new_pcpu_id, '!');
+	}
+    
+    void unlock()
+	{
+#if defined(L4KA_DEBUG_SYNC)
+	    if (cpulock.get_owner_tid() != L4_Myself())
+	    {
+		debug_tid = cpulock.get_owner_tid();
+		debug_pcpu_id = cpulock.get_owner_pcpu_id();
+	    }	
+#endif		
+	    LOCK_ASSERT(cpulock.get_owner_tid() == L4_Myself(), 'a');
+	    LOCK_ASSERT(cpulock.get_owner_pcpu_id() == (word_t) L4_ProcessorNo(), 'b');
+	    LOCK_DEBUG(cpulock.get_owner_pcpu_id(), '^');
+	    release();
+	}
+    
+
+};
+
+
+#endif /* !__L4KA__SYNC_H__ */
