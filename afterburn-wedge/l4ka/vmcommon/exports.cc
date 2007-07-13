1e8e7e1a318f5cfffd9e69a013edd513036bcb88
Jan Stoess <stoess@ira.uka.de>
2007-07-13 13:50:50 +0200

    Remove L4_KDB_Enter

--- afterburn-wedge/l4ka/vmcommon/exports.cc
+++ afterburn-wedge/l4ka/vmcommon/exports.cc
@@ -69,8 +69,7 @@ l4ka_wedge_thread_create(
    if( !hthread )
 	return L4_nilthread;
 
-   //L4_KDB_Enter("Blarb");
-   hthread->start();
+  hthread->start();
 
     return hthread->get_global_tid();
 }
